952fb4ba3bd4ddf78aa8a01d55b0b1173bca99f0
Sruthi Korukonda <Sruthi@Sriharshas-MacBook-Air.local>
2018-09-22 15:42:42 -0400

    first commit

---
+++ lib/prj2.ex
@@ -0,0 +1,18 @@
+defmodule PRJ2 do
+  @moduledoc """
+  Documentation for PRJ2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PRJ2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prj2_test.exs
@@ -0,0 +1,8 @@
+defmodule PRJ2Test do
+  use ExUnit.Case
+  doctest PRJ2
+
+  test "greets the world" do
+    assert PRJ2.hello() == :world
+  end
+end
